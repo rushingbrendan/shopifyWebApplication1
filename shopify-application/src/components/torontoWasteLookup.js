c5406a230a6509969097f83b281a4f27b5be5585
brushing <brushing@outlook.com>
2019-01-12 03:48:24 -0500

    started favourite widget

--- shopify-application/src/components/torontoWasteLookup.js
+++ shopify-application/src/components/torontoWasteLookup.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import './torontoWasteLookup.css';
 
 
 class TorontoWasteLookup extends Component {
 
+    
     constructor(props) {
+        
+        
         super(props);
         this.state = {
             searchInput: '',
@@ -11,7 +15,9 @@ class TorontoWasteLookup extends Component {
             searchKeyword: "egg carton",
             //searchKeyword: "bread bag tag, milk bag tag, elastic band, rubber band, twist tie, rope, twine, string, hemp, ribbon, bow, burlap, staple, fastener, wire, florists wire, plastic tag, tape, duct tape, electrical tape, masking tape, scotch tape, painters tape, tape dispenser, chain, nylon, thread",
             rawData: [],
-            matchedData: []            
+            matchedData: [],
+            favourites: [],
+            selectedFavourite: ''
         };
     }
 
@@ -25,6 +31,20 @@ class TorontoWasteLookup extends Component {
         });
     }
 
+    updateFavourites = (event) => {
+        this.setState({
+            selectedFavourite: event.target.value    
+          });
+
+          if(event.target.checked === true){
+            this.state.favourites.push(event.target.value)      
+          }
+          else{
+            this.state.favourites.pop(event.target.value)      
+          }
+        
+      }
+
     /* Triggered when search button is clicked */
     handleKeywordSearch = (event) => {
         fetch('https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000')
@@ -32,33 +52,119 @@ class TorontoWasteLookup extends Component {
         .then(rawData => this.setState({rawData}));
     }
 
+    getDataFromAPI = (event) => {
+        return(
+            //<div dangerouslySetInnerHTML={{__html: "<p>foo bar</p>"}} />      
+            <div>
+            {this.state.rawData.filter(result=>result.keywords.match(this.state.searchInput)).map(item=>
+            
+              <div className="APIreturnedDataFont" >      
+                  <table border="1">
+                      <tbody>
+                          <td width="5" align="right">
+                              <input type="checkbox" value={item.title} onChange={this.updateFavourites} ></input>
+                          </td>
+                          <td width ="500 px" align="left">
+                              <h4>&nbsp;&nbsp;{item.title}</h4>
+                          </td>                    
+                          <td width="700 px" align="left">
+                          
+                            <div dangerouslySetInnerHTML={{
+                                __html: item.body.split('&lt;').join('<').split('&gt;').join('>')
+                                }} />                                                          
+                          </td>      
+                      </tbody>
+                  </table>                      
+              </div>
+            )}      
+      </div>
+        )
+    }
+
+
+
+    displayFavourites = (event) => {
+        return(
+            //<div dangerouslySetInnerHTML={{__html: "<p>foo bar</p>"}} />      
+            <div>
+            {this.state.rawData.filter(result=>result.keywords.match(this.state.favourites.filter())).map(item=>
+            
+              <div className="APIreturnedDataFont" >      
+                  <table border="1">
+                      <tbody>
+                          <td width="5" align="right">
+                              <input type="checkbox" value={item.title} onChange={this.updateFavourites} ></input>
+                          </td>
+                          <td width ="500 px" align="left">
+                              <h4>&nbsp;&nbsp;{item.title}</h4>
+                          </td>                    
+                          <td width="700 px" align="left">
+                          
+                            <div dangerouslySetInnerHTML={{
+                                __html: item.body.split('&lt;').join('<').split('&gt;').join('>')
+                                }} />                                                          
+                          </td>      
+                      </tbody>
+                  </table>                      
+              </div>
+            )}      
+      </div>
+        )
+    }
+
+
+
+
+    
+
   render() {
+
+      
     return (
       <div className="App">
+      <br className ="spaceAboveHeader"></br>
+      <div className="headerBox">
       <h1>Toronto Waste Lookup</h1>
-
-      
-      
-      <div>
-      <input type="text" value={this.state.searchInput} onChange={event => this.updateSearchInput(event)}></input>
-      <button id="searchButton" onClick={this.handleKeywordSearch}>Search</button>
       </div>
-      
-      {this.state.rawData.filter(result=>result.keywords.match(this.state.searchInput)).map(item=>
-      
-        <div>
-            <h2>{item.title}</h2>
-            <div>
-                {item.body.split('&lt;').join('<').split('&gt;').join('>')}
+      <br className ="spaceUnderHeader"></br>
 
-            </div>
+      <div>
+      <input type="text" className="searchInput" value={this.state.searchInput} onChange={event => this.updateSearchInput(event)}></input>
+            
+      <button id="searchButton" className="searchButton" onClick={this.handleKeywordSearch}>Search</button>
+      </div>
+
+
+      <br className ="spaceUnderHeader"></br>
+
+      <div>
+      {this.state.favourites.map(favourites=>
+            
+        <div className="APIreturnedDataFont" > 
+        {favourites}     
+
+            
+
         </div>
-      )}
+        )}
+
+        </div>
+
+
+      {this.getDataFromAPI()}
+
+      
+      
+
+
 
 
       </div>
     );
+    
   }
+
+  
 }
 
 export default TorontoWasteLookup;
