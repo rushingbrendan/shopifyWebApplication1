11b59c0342a5937e699c65063629116c741174c2
brushing <brushing@outlook.com>
2019-01-15 22:16:11 -0500

    finished commenting and fixed empty search box = no results

--- shopify-application/src/components/torontoWasteLookup.js
+++ shopify-application/src/components/torontoWasteLookup.js
@@ -1,6 +1,18 @@
+//React Component
 import React, { Component } from 'react';
+
+//CSS for App
 import './torontoWasteLookup.css';
-//import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css';
+
+/*  FontAwesomesome Icon Library used for star icon and search icon
+
+star icon: https://fontawesome.com/icons/star?style=solid
+search icon: https://fontawesome.com/icons/search?style=solid
+
+This icon is licensed under the Creative Commons Attribution 4.0 International license.
+
+License Link: https://fontawesome.com/license
+*/
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
@@ -10,19 +22,11 @@ library.add(faStar);
 library.add(faSearch);
 
 
+
 /*
-
-TO DO:
-
-
-
-    3. align plain text
-
-
-
-
+    Class name: TorontoWasteLookup
+    Purpose:    Display Toronto waste data from API on webpage
 */
-
 class TorontoWasteLookup extends Component {
 
     
@@ -31,67 +35,137 @@ class TorontoWasteLookup extends Component {
         
         super(props);
         this.state = {
-            searchInput: '',            
-            searchKeyword: '',            
-            rawData: [],
-            matchedData: [],
-            favourites: [],
-            selectedFavourite: ''
-        };
+            //text from search input
+            searchInput: '',        
 
-        //this.updateFavourites = this.updateFavourites.bind(this);
+            //user keyword used to filter API data    
+            searchKeyword: '',            
+
+            //data from API
+            rawData: [],   
+
+            //user added favourites         
+            favourites: [],            
+        };        
     }
 
+    /* 
+        Function name: componentDidMount
+        
+        Purpose:    Function is called when page has been loaded.
+                    Data is received from the toronto waste services API.
+
+        Parameters:     none
+        Return:         none                
+    */    
     componentDidMount(){
+        //Fetch data from Toronto waste services API
         fetch('https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000')
+        
+        //Complete promise to receive body text as JSON object
         .then(res => res.json())
+
+        //Add JSON object to rawData array in state
         .then(rawData => this.setState({rawData}));
     }
 
+    /* 
+        Function name: updateSearchInput
+        
+        Purpose:    Function is called when the text in the input search box has changed.
+                    The value from the event is put into searchInput variable in class state.
+                
+        Parameters:     event
+        Return:         true
+    */
     updateSearchInput(event) {
 
         this.setState({
           searchInput: event.target.value
         });
-        console.log("in updateSearchInput: "+event.target.value);
-
         
+        return true;
     }
 
-    updateFavourites = event => {        
+    /* 
+        Function name: updateFavourites
+        
+        Purpose:    Function is called when the user clicks on one of the favourite icons beside
+                    a waste data item.
+                    The item's title is collected and added to the favourites array if it does not 
+                    already exist.
+                    If the item is already in the favourites array then it is removed.
+                
+        Parameters:     event
+        Return:         true                
+    */
+    updateFavourites = event => {  
+
         this.setState({
-            selectedFavourite: event 
+            selectedFavourite: event
           });
+        
           
-          if (this.state.favourites.includes(event)){
-            // Find and remove item from an array
+        //Check if selected item is already in array
+        if (this.state.favourites.includes(event)){
+            //Item is found in array
             var i = this.state.favourites.indexOf(event);
             if(i !== -1) {
+                //remove item from array
                 this.state.favourites.splice(i, 1);
             }  
           }
+          //Item is not in array
           else{
+            //add item to array
             this.state.favourites.push(event);   
           }
+          return true;
     }
 
-    /* Triggered when search button is clicked */
+     /* 
+        Function name: submitForm
+        
+        Purpose:    Function is used to pass the value from the search input box to the searchKeyword variable.
+                    The searchKeyword variable determines the data that is shown on the screen from the waste data API.
+
+                    This function is called:
+                        -User presses enter key in search input box
+                        -User clears text in search input box
+                        -User clicks search button
+
+        Parameters:     event
+        Return:         true                
+    */
     submitForm = (event) => {
 
         this.setState({
             searchKeyword: this.state.searchInput
           });
-        
-          console.log("in submit form. keyword: " + this.state.searchKeyword);
-
-        
-
+          
+          return true;
     }
 
+    /* 
+        Function name: getDataFromAPI
+        
+        Purpose:    Function is used to display data from the Toronto waste data API.
+                    The data displayed must have keywords matching the user input in search box.
+                    The waste data title, and body are displayed.
+                    The user must perform a search to view the data.
+                    A star icon is placed next to each item for the user to press to add/remove the item to favourites list.
+                    The data in the body is received from the API in HTML format. The following items are changed.
+                    &lt; converted to <
+                    &gt; converted to >
+                    &nbsp; converted to single white space
+                                                                                                    
+        Parameters:     event
+        Return:         true                
+    */
     getDataFromAPI = (event) => {
-
-
-        if (this.state.searchKeyword !== ""){
+    
+        //make sure that searchKeyword is not blank
+        if ((this.state.searchKeyword !== "") && (this.state.searchInput !== "")){
             return(
                 <div>
                 {this.state.rawData.filter(result=>result.keywords.includes(this.state.searchKeyword)).map(item=>            
@@ -123,20 +197,34 @@ class TorontoWasteLookup extends Component {
         }
     }
 
-
-
-
+    /* 
+        Function name: displayUserFavourites
+        
+        Purpose:    Function is used to display data from the Toronto waste data API that the user has added to favourites list.
+                    The data displayed must have keywords matching the user input in search box.
+                    The waste data title, and body are displayed.
+                    The user must perform a search to view the data.
+                    A star icon is placed next to each item for the user to press to add/remove the item to favourites list.
+                    The data in the body is received from the API in HTML format. The following items are changed.
+                    &lt; converted to <
+                    &gt; converted to >
+                    &nbsp; converted to single white space
+                                                                                                    
+        Parameters:     event
+        Return:         true                
+    */
     displayUserFavouritesFunction = (event) => {
+        
+
+        //only display if there is more than 1 favourite
         if ((this.state.favourites.length) > 1){
             return(
                 <div>
                         <div className="favouritesBackground" >
                         <div className ="searchFrame">
-                    
-                    
+                                        
                     {this.displayFavouritesTitle()}
                     
-
                 {this.state.rawData.filter(result=>this.state.favourites.includes(result.title)).map(item=>                                                        
                     <div className="alignFavouriteData">                                      
                         <div className="checkboxColumn">
@@ -144,8 +232,7 @@ class TorontoWasteLookup extends Component {
                             
                             <FontAwesomeIcon size="1x" icon="star" color="#2B985E"
                             onClick={() => this.updateFavourites(item.title)} />                                
-                            
-                            
+                                                        
                         </div>
                         <div className="titleColumn">
                             <h4>{item.title}</h4>
@@ -174,7 +261,16 @@ class TorontoWasteLookup extends Component {
         }    
     }
 
+    /* 
+        Function name: displayFavouritesTitle
+        
+        Purpose:    Function is used to display the user favourites title heading
+                                                                                                    
+        Parameters:     event
+        Return:         favourites title
+    */    
     displayFavouritesTitle = (event) => {
+        //only display if there are more than 1 favourites
         if ((this.state.favourites.length) > 1){
             return(
                 <div className="favouritesContainer">
@@ -194,7 +290,16 @@ class TorontoWasteLookup extends Component {
 
     
 
-
+    /* 
+        Function name: determineStarType
+        
+        Purpose:    Function is toggle the star icons className.
+                    The icon is grey if the item is not in the user's favourites list.
+                    The icon is green if the item is in the user's favourites list.
+                                                                                                    
+        Parameters:     itemTitle
+        Return:         className      
+    */
     determineStarType(itemTitle) {
         if (this.state.favourites.includes(itemTitle)){
             //make star green
@@ -206,61 +311,55 @@ class TorontoWasteLookup extends Component {
         }
     }
 
-
+    /* 
+        Function name: render
+        
+        Purpose:    Function is used to render the entire Toronto wasta data app.
+                                                                                                    
+        Parameters:     none
+        Return:         waste data app                
+    */
   render() {
 
       
     return (
         
       <div className="App">
-        
-
-
-
-        
+            
         <div className="headerBox">
-        <h1 className="headerTitle">Toronto Waste Lookup</h1>
-        </div>
-        
-        <div className ="spaceUnderHeader"></div>
+            <h1 className="headerTitle">Toronto Waste Lookup</h1>
+        </div>        
 
+        <div className ="spaceUnderHeader"></div>
         <div className ="searchFrame">
-            <input type="text" className="searchInput"  placeholder="Enter search keyword..."
+
+            <input type="text" className="searchInput"  placeholder="Enter search keyword..." 
                 onChange={event => this.updateSearchInput(event)}
                 onKeyDown={event => {
-                    console.log("searchInputLength " + this.state.searchInput.length)
-                    
                     if (event.target.value === ''){
                         this.submitForm()
-                    }
-                    else if (event.key === 'Enter') {
-                      this.submitForm()}
-                      else if (event.key === 'Backspace'){
-                          console.log("backspace pressed - searchInputLength: "+ this.state.searchInput.length)                          
-                      }
+                    }                                        
+                    else if (event.key === 'Enter') {                        
+                      this.submitForm();
+                    }                      
                   }}>
-            </input>     
-                     
+            </input> 
+
             <button className="searchButton" id="searchButton" type="button" onClick={this.submitForm}>
                 <i className="fa fa-search fa-4x"></i>
             </button>
         </div>
-
         
         <div className ="searchFrame">
             {this.getDataFromAPI()}
             <br className ="spaceUnderWasteData"></br>
         </div>
 
-                {this.displayUserFavouritesFunction()}
+        {this.displayUserFavouritesFunction()}
                 
-            </div>
-
-    );
-    
-  }
-
-  
+        </div>
+    );    
+  }  
 }
 
 export default TorontoWasteLookup;
